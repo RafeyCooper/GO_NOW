d2dd59e47663f87d0f59a66943d6a4c7c9de50ce
Muhammad Umair <muhammadumair25591@gamil.com>
2024-12-08 23:40:53 +0500

    update site

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,5 @@
 AOS.init();
+
 const swiper = new Swiper('.swiper-container', {
     slidesPerView: 1.5,
     spaceBetween: 0,
@@ -15,23 +16,42 @@ const swiper = new Swiper('.swiper-container', {
             slides.forEach(slide => {
                 slide.style.transform = 'scale(0.8)';
             });
-            let activeSlide = document.querySelector('.swiper-slide-active');
-            activeSlide.style.transform = 'scale(1)';
+            let activeSlide = document.querySelectorAll('.swiper-slide-active');
+            activeSlide.forEach(slide => {
+                slide.style.transform = 'scale(1)';
+            });
+            // activeSlide.style.transform = 'scale(1)';
         },
         slideChangeTransitionStart: function () {
             let slides = document.querySelectorAll('.swiper-slide');
             slides.forEach(slide => {
                 slide.style.transform = 'scale(0.8)';
             });
-            let activeSlide = document.querySelector('.swiper-slide-active');
-            activeSlide.style.transform = 'scale(1)';
+            let activeSlide = document.querySelectorAll('.swiper-slide-active');
+            activeSlide.forEach(slide => {
+                slide.style.transform = 'scale(1)';
+            });
         }
     }
 });
 
 
 $(document).ready(function() {
+    // Open sidebar
     $('.hamburger-icon-box').on('click', function() {
-        $('.dropdown-menu-custom').stop(true, true).slideToggle(300); // Smooth toggle
+        $('.sidebar-custom').addClass('show');
+        $('.overlay').addClass('show'); // Show overlay
+    });
+
+    // Close sidebar when close button is clicked
+    $('.close-btn').on('click', function() {
+        $('.sidebar-custom').removeClass('show');
+        $('.overlay').removeClass('show'); // Hide overlay
+    });
+
+    // Close sidebar when clicking outside (on overlay)
+    $('.overlay').on('click', function() {
+        $('.sidebar-custom').removeClass('show');
+        $(this).removeClass('show'); // Hide overlay
     });
 });
